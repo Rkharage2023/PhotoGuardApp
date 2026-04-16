eee7367fb67b0078571a7ae19f979dba57630ad9
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2026-04-16 16:33:56 +0530

    changes

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Doctor from "../models/Doctor.js";
+import Doctor from "../models/doctor.js";
 import { protect, restrictTo } from "../middleware/auth.js";
 
 const router = express.Router();
